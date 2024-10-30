db5d80c94875751b3278e15ad0dc3bc39e7c58e5
Vu Quy Tuan <tuan.vq@savvycomsoftware.com>
2024-10-30 17:56:49 +0700

    init code

---
+++ CleanArchWithCQRS/CleanArchWithCQRS.Application/ConfigureServices.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchWithCQRS.Application;
+
+public static class ConfigureServices
+{
+   public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+   {
+      services.AddAutoMapper(Assembly.GetExecutingAssembly());
+      services.AddMediatR(ctg =>
+      {
+         ctg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+      });
+
+      return services;
+   }
+}
